aea22288084f2265dec6edbe8561123429e3fae7
temihren <gusartus@gmail.com>
2020-10-22 16:34:47 +0300

    should work

--- server.js
+++ server.js
@@ -1,41 +1,17 @@
 const express = require('express');
 const path = require('path');
 const WebSocket = require('ws');
+const http = require("http")
+
+const port = process.env.PORT || 3000;
 
 const rootPath = path.join(__dirname, '/');
 
-const app = express();
-
-const messagesWS = new WebSocket.Server({
-	path: '/messages',
-	port: 8080,
-	perMessageDeflate: {
-		threshold: 1024,
-	}
-});
-
 let messages = [];
 
-const pushNewMessage = (message) => new Promise(resolve => {
-	const newMessages = messages.slice();
-	if (newMessages.length > 5) {
-		newMessages.shift();
-	}
-	newMessages.push(message);
+const app = express();
 
-	resolve(newMessages);
-});
-
-messagesWS.on('connection', (socket) => {
-	socket.on('message', (data) => {
-		pushNewMessage(data).then(newMessages => {
-			socket.send(JSON.stringify(newMessages));
-			messages = newMessages;
-		});
-	});
-});
-
-
+const server = http.createServer(app);
 
 app.get('/getLastMessages', (req, res) => {
 	res.status(200).json(messages);
@@ -44,7 +20,27 @@ app.get('/getLastMessages', (req, res) => {
 app.use(express.static(rootPath));
 
 app.get('*', (req, res) => {
-	res.sendFile(rootPath + 'index.html');
+	res.sendFile(rootPath + '/index.html');
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('DA'));
+server.listen(port);
+
+const messagesWS = new WebSocket.Server({server});
+
+messagesWS.on('connection', (socket) => {
+	socket.on('message', (data) => {
+		messages.push(data);
+		messagesWS.clients.forEach(function each(client) {
+			if (client.readyState === WebSocket.OPEN) {
+				client.send(data);
+			}
+		});
+	});
+	socket.on('error', (er) => {
+		console.log(er);
+	});
+});
+
+messagesWS.on('error', (er) => {
+	console.log(er);
+});
